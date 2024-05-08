f17aa79a3222efab775a4f5b039dc5e3fe1f4c43
Fanyam Terkuma <lawrencefanyam@gmail.com>
2024-05-08 17:00:33 +0100

    TAILWIND CSS AND SVG LOADER  SETUP

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,20 @@
 export default {
     content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
-        colors: {
-            primary: '#34caa5',
-            secondary: '#3A3F51',
-            success: '#00AC56',
-            error: '#FF1F7D',
-            warning: '#F5BF3D',
-            white: 'FFFFFF',
-            background: '#FDFDFD'
-        },
         fontFamily: {
             jakarta: ['Plus Jakarta Sans', 'sans-serif']
         },
-        extend: {}
+        extend: {
+            colors: {
+                primary: '#34caa5',
+                secondary: '#3A3F51',
+                success: '#00AC56',
+                error: '#FF1F7D',
+                warning: '#F5BF3D',
+                white: 'FFFFFF',
+                background: '#FDFDFD'
+            }
+        }
     },
     plugins: []
 };
