96232cedcedaee4a2f483476cf7a313d1af169ce
Fanyam Terkuma <lawrencefanyam@gmail.com>
2024-05-08 17:00:37 +0100

    Navigation Sidebar setup completed Added needed icons and logo added a bit of reactivity to the nav icons to as to know which is active

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,10 @@
 <template>
-    <HelloWorld msg="Vite + Vue" />
+    <div class="flex">
+        <SideBar class="max-w-1/4" />
+        <main class="w-full">Body</main>
+    </div>
 </template>
 <script setup>
     import HelloWorld from './components/HelloWorld.vue';
+    import SideBar from './components/SideBar.vue';
 </script>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,67 @@
+<template>
+    <section class="h-screen border-r border-gray-200 flex flex-col w-[6%]">
+        <nav class="bg-gray-50 py-4 h-4/5 flex flex-col gap-4 justify-between">
+            <span class="self-center"> <Logo /></span>
+            <ul>
+                <li class="flex justify-end items-center gap-5 w-full">
+                    <MenuIcon @click="setActive('menu')" />
+                    <div class="bg-white p-[1.7px] h-7 rounded-l-lg" :class="{ 'bg-black p-[1.7px] h-7 rounded-l-lg': activeTab === 'menu' }"></div>
+                </li>
+                <li class="flex justify-end items-center gap-5 w-full">
+                    <Arrow @click="setActive('arrow')" />
+                    <div class="bg-white p-[1.7px] h-7 rounded-l-lg" :class="{ 'bg-black p-[1.7px] h-7 rounded-l-lg': activeTab === 'arrow' }"></div>
+                </li>
+                <li class="flex justify-end items-center gap-5 w-full">
+                    <PeopleIcon @click="setActive('peopleIcon')" />
+                    <div class="bg-white p-[1.7px] h-7 rounded-l-lg" :class="{ 'bg-black p-[1.7px] h-7 rounded-l-lg': activeTab === 'peopleIcon' }"></div>
+                </li>
+                <li class="flex justify-end items-center gap-5 w-full">
+                    <PackageIcon @click="setActive('packageIcon')" />
+                    <div class="bg-white p-[1.7px] h-7 rounded-l-lg" :class="{ 'bg-black p-[1.7px] h-7 rounded-l-lg': activeTab === 'packageIcon' }"></div>
+                </li>
+                <li class="flex justify-end items-center gap-5 w-full">
+                    <PercentageIcon @click="setActive('percentageIcon')" />
+                    <div class="bg-white p-[1.7px] h-7 rounded-l-lg" :class="{ 'bg-black p-[1.7px] h-7 rounded-l-lg': activeTab === 'percentageIcon' }"></div>
+                </li>
+                <li class="flex justify-end items-center gap-5 w-full">
+                    <FailedIcon @click="setActive('failedIcon')" />
+                    <div class="bg-white p-[1.7px] h-7 rounded-l-lg" :class="{ 'bg-black p-[1.7px] h-7 rounded-l-lg': activeTab === 'failedIcon' }"></div>
+                </li>
+            </ul>
+            <ul class="flex flex-col items-center">
+                <li><ArrowRight /></li>
+                <li><SettingsIcon /></li>
+                <li><ArrowLeft /></li>
+            </ul>
+        </nav>
+        <div class="bg-white h-1/5"></div>
+    </section>
+</template>
+
+<script setup>
+    import Logo from '../assets/logo.svg';
+    import Arrow from '../assets/arrow.svg';
+    import MenuIcon from '../assets/menuIcon.svg';
+    import PeopleIcon from '../assets/PeopleIcon.svg';
+    import PercentageIcon from '../assets/percentageIcon.svg';
+    import PackageIcon from '../assets/packageIcon.svg';
+    import ArrowLeft from '../assets/arrow-left.svg';
+    import FailedIcon from '../assets/failed.svg';
+    import ArrowRight from '../assets/arrow-right.svg';
+    import SettingsIcon from '../assets/gearIcon.svg';
+    import { ref } from 'vue';
+
+    let activeTab = ref('menu');
+
+    const setActive = (tab) => {
+        if (activeTab === tab) {
+            return;
+        }
+        activeTab.value = tab;
+    };
+</script>
+<style scoped>
+    li {
+        cursor: pointer;
+    }
+</style>
