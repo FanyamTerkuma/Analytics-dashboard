91432c45602ac1b8f97a6e92e9f22f05fcc51c84
Fanyam Terkuma <lawrencefanyam@gmail.com>
2024-05-16 18:47:43 +0100

    Modified Styling to suit figma design

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,14 @@
         <SideBar class="max-w-1/4" />
         <main class="w-full flex flex-col relative">
             <DashboardHeader class="absolute z-50 shadow-md backdrop-blur-xl bg-white" />
-            <section class="pt-24 z-10 p-4 w-full max-h-screen overflow-scroll grid grid-cols-2">
-                <div class="col-span-1 grid grid-cols-1 grid-rows-2 gap-4"><Chart /><Chart /></div>
-                <div class="p-2">orders</div>
+            <section class="bg-[#FAFAFA] space-y-4 pt-24 z-10 py-4 px-3 w-full h-screen overflow-scroll">
+                <div class="grid grid-cols-3">
+                    <Chart class="col-span-2" />
+                    <AnalyticsCardList class="col-span-1" />
+                </div>
+                <div>
+                    <div>2</div>
+                </div>
             </section>
         </main>
     </div>
@@ -14,4 +19,5 @@
     import DashboardHeader from './components/DashboardHeader.vue';
     import SideBar from './components/SideBar.vue';
     import Chart from './components/Chart.vue';
+    import AnalyticsCardList from './components/AnalyticsCardList.vue';
 </script>
